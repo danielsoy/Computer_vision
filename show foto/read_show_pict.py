c5f3f8b53a7a15dd87fdea7393031c22b0a99516
danielsoy <48926149+danielsoy@users.noreply.github.com>
2024-10-24 16:50:29 -0300

    read_show_pict.py

--- show foto/read_show_pict.py
+++ show foto/read_show_pict.py
@@ -1,14 +1,14 @@
+import cv2 as cv  # import opencv2 as cv.you need to install it before to import it.
 
-import cv2 as cv  # import opencv2 as cv.you need to install it before import it.
-
-############################
+###################
 
 Image = cv.imread("C:\\anodet/notebooks\show foto/andrea pietra.jpg") # path to the picture to read and show.
 
-cv.imshow("andrea", Image)
+cv.imshow("andrea", Image) # chose a name for the window that will hold the image, and show it.
 
-cv.waitKey(0)             # (0) waits 4 ever. click space bar to continue.
+cv.waitKey(0)              # (0) waits 4 ever. click any key to continue.
                            
-cv.destroyAllWindows()    # destroy all open windows. 
+cv.destroyAllWindows()     # destroy all open windows. 
 
-print("Listo el pollo!")
+print("Listo el pollo!")   # chicken ready!
+
